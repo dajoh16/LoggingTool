46332d0d2b180a7108fda733ac478a6ad48b1e08
Hugzy <frede.madsen3@gmail.com>
2016-11-30 19:06:54 +0100

    Made loadRaid

---
+++ src/backend/LoadRaid.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author frede
+ */
+public class LoadRaid implements LoadFile {
+
+    @Override
+    public ObservableList loadObservableList(String fileToRead) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
